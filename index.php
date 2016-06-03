ef10e6ed7fa8121dcae73904645f53019b388252
51scpa1mst <51scpa1mst@hft-stuttgart.de>
2016-06-03 12:14:12 +0200

    Print php config

--- index.php
+++ index.php
@@ -2,6 +2,26 @@
 echo "<html>";
 
 echo "Under construction.";
+phpinfo();
+
+/* $connectionInfo = array("UID" => "HftAdmin@hft-projects", "pwd" => "", "Database" => "EnglishTraining", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:hft-projects.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
+
+if($conn){
+	echo "Connection established.<br />";
+}
+else{
+	echo "Connection could not be established.<br />";
+	die( print_r( sqlsrv_errors(), true));
+}
+*/
+//-----------------------------------------------
+// Perform operations with connection.
+//-----------------------------------------------
+
+/* Close the connection. */
+//sqlsrv_close( $conn);
 
 echo "</html>"
 ?>
