038ccdcacf80f3e9960931b071cfcb63cacf2df1
51scpa1mst <51scpa1mst@hft-stuttgart.de>
2016-06-03 13:25:20 +0200

    added db_access file and test query

---
+++ db_access.php
@@ -0,0 +1,43 @@
+<?php
+function OpenConnection() {
+	$connectionInfo = array (
+			"UID" => "HftAdmin@hft-projects",
+			"pwd" => "",
+			"Database" => "EnglishTraining",
+			"LoginTimeout" => 30,
+			"Encrypt" => 1,
+			"TrustServerCertificate" => 0
+	);
+	$serverName = "tcp:hft-projects.database.windows.net,1433";
+	$conn = sqlsrv_connect ( $serverName, $connectionInfo );
+
+	if ($conn) {
+		echo "Connection established.<br />";
+	} else {
+		echo "Connection could not be established.<br />";
+		die ( print_r ( sqlsrv_errors (), true ) );
+	}
+
+	return $conn;
+}
+
+function ReadData() {
+	try {
+		$conn = OpenConnection ();
+		$tsql = "SELECT * FROM Question";
+		$getProducts = sqlsrv_query ( $conn, $tsql );
+		if ($getProducts == FALSE)
+			die ( FormatErrors ( sqlsrv_errors () ) );
+			$productCount = 0;
+			while ( $row = sqlsrv_fetch_array ( $getProducts, SQLSRV_FETCH_ASSOC ) ) {
+				echo ($row ['Text']);
+				echo ("<br/>");
+				$productCount ++;
+			}
+			sqlsrv_free_stmt ( $getProducts );
+			sqlsrv_close ( $conn );
+	} catch ( Exception $e ) {
+		echo ("Error!");
+	}
+}
+?>
--- index.php
+++ index.php
@@ -1,27 +1,13 @@
 <?php
+include 'db_access.php';
+
 echo "<html>";
 
 echo "Under construction.";
-phpinfo();
+phpinfo ();
+ReadData ();
 
-$connectionInfo = array("UID" => "HftAdmin@hft-projects", "pwd" => "", "Database" => "EnglishTraining", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-$serverName = "tcp:hft-projects.database.windows.net,1433";
-$conn = sqlsrv_connect($serverName, $connectionInfo);
+echo "</html>";
 
-if($conn){
-	echo "Connection established.<br />";
-}
-else{
-	echo "Connection could not be established.<br />";
-	die( print_r( sqlsrv_errors(), true));
-}
 
-//-----------------------------------------------
-// Perform operations with connection.
-//-----------------------------------------------
-
-/* Close the connection. */
-sqlsrv_close( $conn);
-
-echo "</html>"
 ?>
