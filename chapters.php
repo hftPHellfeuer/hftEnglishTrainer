f217b5efc2d37f8bfd32a6f526218d98f39b61c3
51scpa1mst <51scpa1mst@hft-stuttgart.de>
2016-06-10 13:51:22 +0200

    Added chapters.php

---
+++ chapters.php
@@ -0,0 +1,38 @@
+<?php
+include 'db_access.php';
+function getAllChapters() {
+	
+	$sql = "SELECT * FROM Chapter";
+	
+	$conn = OpenConnection ();
+	
+	$stmt = sqlsrv_query ( $conn, $sql );
+	if ($stmt == false) {
+		echo "Error in executing statement.\n";
+		die ( print_r ( sqlsrv_errors (), true ) );
+	}
+	
+	$chapters = array ();
+	
+	if ($stmt == false) {
+		echo "Could not load chapters.\n";
+		die ( print_r ( sqlsrv_errors (), true ) );
+	} else {
+		$count = 0;
+		while ( $row = sqlsrv_fetch_array ( $stmt, SQLSRV_FETCH_ASSOC ) ) {
+			array_push ( $chapters, array (
+					'Id' => $row ['Id'],
+					'Name' => $row ['Name'],
+					'Description' => $row ['Description'] 
+			) );
+			$count ++;
+		}
+	}
+	
+	/* Free the statement and connection resources. */
+	sqlsrv_free_stmt ( $stmt );
+	CloseConnection ( $conn );
+	
+	return $chapters;
+}
+?>
