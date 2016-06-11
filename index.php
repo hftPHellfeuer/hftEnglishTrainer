3c8423b29c74011be141221ac0871349bc217707
51scpa1mst <51scpa1mst@hft-stuttgart.de>
2016-06-11 12:13:21 +0200

    Added teacher login.

--- index.php
+++ index.php
@@ -2,25 +2,23 @@
 <?php
 include_once 'chapters.php';
 include_once 'navigationBar.php';
-		
-		navigationBar();
 
-	echo ('<div class="col-md-8 col-md-offset-2">');
-	if (isLogedIn())
-	{
+navigationBar ();
 
-		echo "<h1>Chapters</h1>";
-		$chapters = getAllChapters(getUserId());
-		
-		if ($chapters == null)
-		{
-			echo "There are currently no chapters unlocked for you. Please ask your teacher. <br/> At the moment it is not possible to start the quiz.";
-		} else{
+echo ('<div class="col-md-8 col-md-offset-2">');
+if (isLogedIn ()) {
+	
+	echo "<h1>Chapters</h1>";
+	$chapters = getAllChapters ( getUserId () );
+	
+	if ($chapters == null) {
+		echo "There are currently no chapters unlocked for you. Please ask your teacher. <br/> At the moment it is not possible to start the quiz.";
+	} else {
 		foreach ( $chapters as $chapter ) {
 			echo '<p><b>' . $chapter ['Id'] . ')</b><u>' . $chapter ['Name'] . "</u>: " . $chapter ['Description'] . '</p>';
 		}
 		
-		//TODO use combobox?
+		// TODO use combobox?
 		
 		echo "<h1>Start the game</h1>";
 		echo '	<form name="form" action="question_show.php" method="get">
@@ -28,14 +26,11 @@ include_once 'navigationBar.php';
 				<input type="hidden" name="i" value="0"><br/>
 				<input class=\'btn btn-success btn-lg\' type="submit" value="Start the Quiz!"><br/>
 			</form>';
-		}
-
-	}else
-	{
-		echo "Welcome <br> Please Log In before playing <br> <div class='btn btn-success btn-lg'> <a href='login.php'> Log In </a></div>";
 	}
-	echo ('</div>');
-
+} else {
+	echo "Welcome <br> Please Log In before playing <br> <div class='btn btn-success btn-lg'> <a href='login.php'> Log In </a></div>";
+}
+echo ('</div>');
 
 ?>
 </html>
--- login.php
+++ login.php
@@ -29,9 +29,13 @@ if (isset ( $errorMessage )) {
 if (isset ( $_GET ['login'] )) {
 	$email = $_POST ['email'];
 	$passwort = $_POST ['passwort'];
-	
-	$result = loginStudent ( $email, $passwort );
-	
+
+	if (isset($_POST ['isTeacher'])){
+		$result = loginTeacher( $email, $passwort );
+	} else{
+		$result = loginStudent ( $email, $passwort );
+	}
+
 	if ($result == null) {
 		echo "<div class='jumbotron col-md-6 col-md-offset-3' > <h3>User / password wrong</h3></div>";
 	} else {
@@ -50,6 +54,10 @@ if (isset ( $_GET ['login'] )) {
 				<input type="password" name="passwort" placeholder="Password"
 					class="form-control">
 			</div>
+			<div class="form-group">
+				<input type="checkbox" name="isTeacher">Login as Teacher</input>
+			</div>
+			
 			<br>
 			<button type="submit" class="btn btn-success btn-lg">Log in</button>
 		</form>
